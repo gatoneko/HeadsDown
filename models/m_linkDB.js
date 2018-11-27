095f0f1225617695f9fdd30f797ac84b86cd1411
Conrad Yosuke <me@private.com>
2018-11-27 14:22:15 -0600

    can select random unused link

--- models/m_linkDB.js
+++ models/m_linkDB.js
@@ -16,6 +16,34 @@ function linkDB() {
 			})
 		}
 	}
+	this.activateLinkPair = async function() {
+		var result = []
+		var pollLink = await this.getUnusedLink();
+		var adminLink = await this.getUnusedLink();
+		result.push(pollLink);
+		result.push(adminLink);
+		return result;
+	}
+
+	this.getUnusedLink = async function() {
+		var unusedLinks = await LinkModel.find({inUse: false}).exec();
+		var count = unusedLinks.length;
+		console.log("count: " + count);
+		var result = this.selectRandomDoc(unusedLinks, count);
+		result.inUse = true;
+		await result.save();
+		return result
+	}
+
+	this.selectRandomDoc = function(unusedLinks, max) {
+		return unusedLinks[getRandomInt(max)];
+	}
+
+}
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 module.exports = new linkDB();
--- models/m_linkDBTest.js
+++ models/m_linkDBTest.js
@@ -4,6 +4,13 @@ var linkDB = require('./m_linkDB');
 dburi = 'mongodb://datrukup:pokemon1@ds211694.mlab.com:11694/polls';
 mongoose.connect(dburi);
 
-linkDB.populateDB();
+// linkDB.populateDB();
+
+async function a() {
+	var result = await linkDB.activateLinkPair();
+	console.log(result);
+}
+
+a();
 
 // process.exit(1);
